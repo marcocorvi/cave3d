6fc078a3493c64001544abf7b89c713724825fee
marcocorvi <marco.corvi@gmail.com>
2019-04-18 10:33:37 +0200

    fractal computer

---
+++ src/com/topodroid/Cave3D/FractalDialog.java
@@ -0,0 +1,98 @@
+/* @file FractalDialog.java
+ *
+ * @author marco corvi
+ * @date mar 2018
+ *
+ * @brief Cave3D fractal counts dialog
+ * --------------------------------------------------------
+ *  Copyright This sowftare is distributed under GPL-3.0 or later
+ *  See the file COPYING.
+ */
+package com.topodroid.Cave3D;
+
+import android.os.Bundle;
+import android.app.Dialog;
+// import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+
+import android.graphics.*;
+import android.view.View;
+import android.view.ViewGroup.LayoutParams;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+import android.widget.CheckBox;
+import android.widget.RadioButton;
+
+// import android.util.Log;
+
+public class FractalDialog extends Dialog 
+                           implements View.OnClickListener
+{
+  private Cave3D   mCave3d;
+  private Context  mContext;
+  private CheckBox mCBsplays;
+  private Button mBtnOk;
+  private ImageView mImage;
+  // private Button mBtnClose;
+  private EditText mCell;
+  private static int mCellSide = 2;
+
+  private Cave3DRenderer mRenderer;
+  private RadioButton mRBtotal;
+  private RadioButton mRBnghb;
+
+  public FractalDialog( Context context, Cave3D cave3d, Cave3DRenderer renderer )
+  {
+    super( context );
+    mContext  = context;
+    mCave3d   = cave3d;
+    mRenderer = renderer;
+  }
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState)
+  {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.fractal_dialog);
+    getWindow().setLayout( LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT );
+
+    TextView tv = ( TextView ) findViewById(R.id.fractal_count);
+    tv.setText( FractalResult.countsString() );
+
+    tv = (TextView) findViewById( R.id.fractal_computer );
+    tv.setText( (FractalResult.computer != null)? "fractal computer is running" : "fractal computer is idle" );
+
+    mBtnOk = (Button) findViewById( R.id.fractal_ok );
+    mBtnOk.setOnClickListener( this );
+
+    mCell = (EditText) findViewById( R.id.fractal_cell );
+    mCell.setText( Integer.toString( mCellSide ) );
+
+    mCBsplays = (CheckBox) findViewById( R.id.fractal_splays );
+    mImage    = (ImageView) findViewById( R.id.fractal_dims );
+
+    mImage.setImageBitmap( FractalResult.makeImage() );
+
+    mRBtotal = (RadioButton) findViewById( R.id.fractal_count_total );
+    mRBnghb  = (RadioButton) findViewById( R.id.fractal_count_nghb  );
+
+    setTitle( R.string.fractal_title );
+  }
+
+  @Override
+  public void onClick(View view)
+  {
+    // Log.v( TAG, "onClick()" );
+    mCellSide = Integer.parseInt( mCell.getText().toString() );
+    int mode = FractalComputer.COUNT_TOTAL;
+    if ( mRBnghb.isChecked() ) mode = FractalComputer.COUNT_NGHB;
+    int ret = FractalResult.compute( mContext, mCave3d, mRenderer, mCBsplays.isChecked(), mCellSide, mode );
+    dismiss();
+  }
+
+}
+
---
+++ src/com/topodroid/Cave3D/FractalResult.java
@@ -0,0 +1,94 @@
+/** @file FractalResult.java
+ *
+ *e @author marco corvi
+ * @date nov 2011
+ *
+ * @brief Cave3D model renderer
+ * --------------------------------------------------------
+ *  Copyright This sowftare is distributed under GPL-3.0 or later
+ *  See the file COPYING.
+ */
+package com.topodroid.Cave3D;
+
+// import java.util.List;
+
+import android.os.AsyncTask;
+import android.content.Context;
+
+import android.widget.Toast;
+import android.graphics.Bitmap;
+
+class FractalResult
+{
+  static FractalComputer computer = null;
+  static Cave3D  mCave3d = null;
+  static Context mContext;
+
+  static float[] mCount = new float[ FractalComputer.SIZE ];
+
+  static int compute( Context context, Cave3D cave3d, final Cave3DRenderer r, boolean do_splays, int cell, int mode )
+  {
+    if ( computer != null ) return -1;
+    mContext = context;
+    mCave3d  = cave3d;
+    computer = new FractalComputer( r.xmin, r.xmax, r.ymax, r.ymin, r.zmin, r.zmax, do_splays, cell, mode );
+    (new AsyncTask<Void, Void, Void>() {
+	public Void doInBackground( Void ... v ) {
+          computer.computeFractalCounts( r.getShots(), r.getSplays() );
+	  return null;
+	}
+    }).execute();
+    return 0;
+  }
+
+  static void releaseComputer()
+  {
+    computer = null;
+    if ( mCave3d != null ) {
+      mCave3d.runOnUiThread( new Runnable() {
+        public void run() {
+          Toast.makeText( mContext, "Fractal compute finished", Toast.LENGTH_SHORT ).show();
+        }
+      } );
+    }
+  }
+
+  static void setCount(int k, float val) { mCount[k] = val; }
+
+  static String countsString()
+  {
+    StringBuilder counts = new StringBuilder();
+    counts.append( Integer.toString( (int)(mCount[0]*100) ) );
+    for ( int s=1; s<FractalComputer.SIZE; ++s ) {
+      counts.append(" ");
+      counts.append( Integer.toString( (int)(mCount[s]*100) ) );
+    }
+    return counts.toString();
+  }
+
+  static Bitmap makeImage()
+  {
+    int ww = 100 * FractalComputer.DIM_ONE + 100;
+    int hh = 500;
+    int zz = 6;
+    int zero = 100;
+    int ux =  50; // unit
+    int uy = 100; // unit
+    Bitmap bitmap = Bitmap.createBitmap( ww, hh, Bitmap.Config.ARGB_8888 );
+    for ( int j=0; j<hh; ++j ) for ( int i=0; i<ww; ++i ) bitmap.setPixel( i, j, 0 );
+    // for ( int j=uy; j<hh; j+=uy ) for ( int i=0; i<ww; ++i ) bitmap.setPixel( i, j, 0xff999999 );
+    for ( int i=0; i<ww; ++i ) bitmap.setPixel( i, hh-zero, 0xffffffff );
+    for ( int i=0; i<ww; ++i ) bitmap.setPixel( i, hh-1*uy-zero, 0xffcccccc );
+    for ( int i=0; i<ww; ++i ) bitmap.setPixel( i, hh-2*uy-zero, 0xff999999 );
+    for ( int i=0; i<ww; ++i ) bitmap.setPixel( i, hh-3*uy-zero, 0xff666666 );
+    for ( int k=0; k<FractalComputer.SIZE; ++k ) {
+      int j = hh - zero - (int)(mCount[k]*uy);
+      int i = ux + ux*k;
+      if ( j > zz && j < hh-zz-1  ) {
+        for ( int jj=j-zz; jj<=j+zz; ++jj) for ( int ii=i-zz; ii<=i+zz; ++ii ) bitmap.setPixel( ii, jj, 0xffff0000 );
+      }
+    }
+    return bitmap;
+  }
+
+}
